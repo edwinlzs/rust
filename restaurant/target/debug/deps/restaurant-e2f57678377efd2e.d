12fe2a230c7d4d12cca8baba4093854749626aed
Edwin Lim <edwinlzs97@gmail.com>
2022-01-27 00:21:33 +0800

    trying out modules

---
+++ restaurant/target/debug/deps/restaurant-e2f57678377efd2e.d
@@ -0,0 +1,7 @@
+C:\Users\edwin\Desktop\Github\rust\restaurant\target\debug\deps\restaurant-e2f57678377efd2e.rmeta: src\lib.rs
+
+C:\Users\edwin\Desktop\Github\rust\restaurant\target\debug\deps\librestaurant-e2f57678377efd2e.rlib: src\lib.rs
+
+C:\Users\edwin\Desktop\Github\rust\restaurant\target\debug\deps\restaurant-e2f57678377efd2e.d: src\lib.rs
+
+src\lib.rs:
---
+++ restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+C:\Users\edwin\Desktop\Github\rust\restaurant\target\debug\librestaurant.rlib: C:\Users\edwin\Desktop\Github\rust\restaurant\src\lib.rs
